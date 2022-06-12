e2ca0bf7cde393048ad2ec48986acb07c26a3dd7
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-13 00:31:03 +0300

    commit tree 301d1f706dd1ecc90617ccf7af46c3624de81546

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const feet = 264;
+
+const distanceFromHqInBlocks = (blocks) => {
+  return Math.abs(blocks - 42);
+};
+
+const distanceFromHqInFeet = (distance) => {
+  return distanceFromHqInBlocks(distance) * feet;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  let distance = Math.abs(start - destination) * feet;
+  return destination < distanceFromHqInBlocks(start)
+    ? distanceFromHqInFeet(destination)
+    : distance;
+};
+
+const calculatesFarePrice = (start, destination) => {
+  let distanceTravelled = distanceTravelledInFeet(start, destination);
+  if (distanceTravelled < 400) {
+    return 0;
+  } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+    return (distanceTravelled - 400) * 0.02;
+  } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+};
+
